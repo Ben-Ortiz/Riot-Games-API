d8caa4e54e3ecb452babae4f373c62554d364cfe
Benedict Ortiz <benortiz1391@gmail.com>
2022-05-02 19:11:51 -0400

    comments

--- RestfulProviderService1/src/main/java/com/ben/controllers/LOLChampionController.java
+++ RestfulProviderService1/src/main/java/com/ben/controllers/LOLChampionController.java
@@ -14,10 +14,13 @@ import com.ben.service.LOLChampionService;
 @RequestMapping("/leagueOfLegends")
 public class LOLChampionController {
 	
+	//http://localhost:8080/leagueOfLegends/...
+	
 	@Autowired
 	private LOLChampionService lcService;
 	
 	//sanity check for my get request to work
+	//http://localhost:8080/leagueOfLegends/championsTest
 	@GetMapping("/championsTest")
 	public String getChampionTest() {
 		LOLChampion lolChampion = new LOLChampion(1, "Viktor");
@@ -25,10 +28,12 @@ public class LOLChampionController {
 		
 	}
 	
+	//http://localhost:8080/leagueOfLegends/champions
 	@GetMapping("/champions")
 	public List<LOLChampion> getChampion() {
 		return lcService.getLOLChampions();
-		
 	}
+	
+	
 
 }
