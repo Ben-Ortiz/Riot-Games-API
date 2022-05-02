a7f6ca3e2b4fc892d7d640a415f50fe8c65f3e45
Benedict Ortiz <benortiz1391@gmail.com>
2022-05-02 18:33:42 -0400

    javadoc

--- RestfulProviderService1/src/main/java/com/ben/RestfulProviderService1Application.java
+++ RestfulProviderService1/src/main/java/com/ben/RestfulProviderService1Application.java
@@ -3,6 +3,12 @@ package com.ben;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author benor
+ *
+ *	This RESTful API provides league of legends champions and their details.
+ */
 @SpringBootApplication
 public class RestfulProviderService1Application {
 
