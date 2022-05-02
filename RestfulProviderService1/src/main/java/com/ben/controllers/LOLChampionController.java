c8c818c4465d9cbd99eadfc2b85cd9a7797f41fd
Benedict Ortiz <benortiz1391@gmail.com>
2022-05-02 18:27:45 -0400

    edits

--- RestfulProviderService1/src/main/java/com/ben/controllers/LOLChampionController.java
+++ RestfulProviderService1/src/main/java/com/ben/controllers/LOLChampionController.java
@@ -1,14 +1,17 @@
 package com.ben.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ben.model.LOLChampion;
 
 @RestController
+@RequestMapping("/leagueOfLegends")
 public class LOLChampionController {
 	
-	@GetMapping("/champions")
+	//sanity check for my post to work
+	@GetMapping("/championsTest")
 	public String getChampion() {
 		LOLChampion lolChampion = new LOLChampion(1, "Viktor");
 		return lolChampion.getChampName();
