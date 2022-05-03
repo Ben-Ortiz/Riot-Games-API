1a62ca911bfbbb2684fbe6b4f92da9a354fb0dc7
Benedict Ortiz <benortiz1391@gmail.com>
2022-05-03 16:24:08 -0400

    soap service scaffolding

---
+++ SoapProviderService1/src/test/java/com/ben/SoapProviderService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapProviderService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
