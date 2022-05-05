dfe54e4631e7276433b917d66eb98c5e9ffba2ab
Benedict Ortiz <benortiz1391@gmail.com>
2022-05-05 01:40:24 -0400

    basic crud consumer made

--- RestfulConsumerService1/src/main/java/com/ben/controller/LOLChampionController.java
+++ RestfulConsumerService1/src/main/java/com/ben/controller/LOLChampionController.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,11 @@ public class LOLChampionController {
 	public LOLChampion saveLOLChampion(@RequestBody LOLChampion lolChampion) {
 		ResponseEntity<LOLChampion> response = restTemplate.postForEntity("http://localhost:8080/leagueOfLegends/postChampion", lolChampion, LOLChampion.class);
 		return response.getBody();
+	}
+	
+	@PutMapping("lolchampion/update/{id}")
+	public void updateLOLChampion(@PathVariable Long id, @RequestBody LOLChampion lolChampion) {
+		restTemplate.put("http://localhost:8080/leagueOfLegends/champions/" + id, lolChampion);
 	}
 	
 	@DeleteMapping("/lolchampion/delete")
